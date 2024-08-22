54a73d161b7ea750e13ed51effb55dfc463b6d76
Augusto Aleman <augustoae09@gmail.com>
2024-08-21 21:59:21 -0600

    Added basic layout and styles for contact page

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as NavbarComponent } from './Navbar';
+export { default as FormulaireComponent } from './Formulaire';
--- portfolio/src/pages/ContactPage.js
+++ portfolio/src/pages/ContactPage.js
@@ -1,25 +1,38 @@
-import logo from '../logo.svg';
-import '../App.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import { FormulaireComponent } from '../components';
+import '../assets/styles/contactpage.css'
 
 function ContactPage() {
   const subject = "Contact";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola, {subject}!
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className='contact-page'>
+        <Col className='contact-text'>
+          <Col className='contact-description'>
+            <h1>Let's Get <span>In Touch!</span></h1>
+            <p>Your thoughts and feedback are important to me! If you’re interested in collaborating on a project, have a question about my work, or just want to get in touch, I’m eager to hear from you. Please fill out the contact form  with your details and message, and I’ll get back to you as soon as I can. Let’s start a conversation and explore how we might work together!</p>
+          </Col>
+          <hr className="separator"></hr>
+          <Col className='contact-social-media'>
+            <h3>Connect with Me</h3>
+            <Row className='contact-social-media-buttons'>
+              <a href='https://www.linkedin.com' style={{ '--j': 1 }}>
+                <i class='bx bxl-linkedin' ></i>
+              </a>
+              <a href='https://www.github.com' style={{ '--j': 2 }}>
+                <i class='bx bxl-github' ></i>
+              </a>
+              <a href = 'https://www.facebook.com' style = {{'--j': 3}}>
+                <i class='bx bxl-facebook' ></i>
+              </a>
+            </Row>
+          </Col>
+        </Col>
+        <Col className='contact-formulaire'>
+            <FormulaireComponent></FormulaireComponent>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
