1ac657a7ae367b110ae4fa3336d5ab2114e35faf
Augusto Aleman <augustoae09@gmail.com>
2024-08-25 21:38:26 -0600

    Added general styles for responsive layout + footer component

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as NavbarComponent } from './Navbar';
 export { default as FormulaireComponent } from './Formulaire';
+export { default as FooterComponent } from './Footer';
--- portfolio/src/pages/ProjectPage.js
+++ portfolio/src/pages/ProjectPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, Button, Image, Badge } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 import projectsData from './sections/data/projectsData.json'; 
+import Carousel from 'react-bootstrap/Carousel';
 import '../assets/styles/project.css';
 
 const importImage = (imageName) => {
@@ -15,6 +17,12 @@ const importImage = (imageName) => {
 function ProjectPage() {
     const { projectId } = useParams();
     const project = projectsData.find(p => p.url === `/projects/${projectId}`);
+
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
 
     if (!project) {
         return <p>Project not found.</p>;
@@ -30,14 +38,29 @@ function ProjectPage() {
         ));
     };
 
+
+
     return (
         <Container>
             <Row className='project-page'>
                 <Col className='project-images'>
-                    <Image src={importImage(project.bannerImage)} alt={project.title} fluid />
-                    <Image src={importImage(project.image2)} alt={`${project.title} - 2`} fluid />
-                    <Image src={importImage(project.image3)} alt={`${project.title} - 3`} fluid />
-                    <Image src={importImage(project.image4)} alt={`${project.title} - 4`} fluid />
+                    <Carousel activeIndex={index} onSelect={handleSelect} className='project-carousel'>
+                        <Carousel.Item>
+                            <img src=  {importImage(project.bannerImage)} />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <img src=  {importImage(project.bannerImage)} />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <img src=  {importImage(project.bannerImage)} />
+                        </Carousel.Item>
+                    </Carousel>
+                    <p>{addBreaks(project.shortDescription)}</p>
+                    <p> 
+                        <i class='bx bx-right-arrow-alt'></i>
+                        &nbsp;To see the source code click 
+                        <a> here.</a>
+                    </p>
                 </Col>
 
                 <Col className='project-data'>
@@ -54,27 +77,23 @@ function ProjectPage() {
                     </Row>
                     <ul className='project-properties'>
                         <li><h5>
-                            Technologies:  
+                            Technologies:&nbsp;&nbsp; &nbsp;   
                             <span>
                                 {project.technologies.join(', ')}
                             </span>
                         </h5></li>
                         <li><h5>
-                            Methodologies:  
+                            Methodologies:&nbsp;&nbsp;&nbsp;    
                             <span>
                                 {project.methodologies.join(', ')}
                             </span>
                         </h5></li>
                         <li><h5>
-                            Developed for: <span> {project.user}</span>
+                            Developed for:&nbsp;&nbsp;&nbsp;   
+                            <span> {project.user}</span>
                         </h5></li>
                     </ul>
                     <p>{addBreaks(project.fullDescription)}</p>
-                    <Row className='project-github'>
-                        <a href='https://www.github.com' style={{ '--j': 2 }}>
-                            <i className='bx bxl-github'></i>
-                        </a>
-                    </Row>
                 </Col>
             </Row>
         </Container>
