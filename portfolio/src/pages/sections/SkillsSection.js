01aca51f1cf7058c178fd2a43edc14040bc4b998
Augusto Aleman <augustoae09@gmail.com>
2024-09-06 19:15:09 -0600

    Solved console errors

--- portfolio/src/pages/sections/SkillsSection.js
+++ portfolio/src/pages/sections/SkillsSection.js
@@ -38,19 +38,18 @@ function SkillSection() {
           <h1>My <span>Skills</span></h1>
           <div className='skill-cards'>
             {skillsData.map((skill, index) => (
-                <Card className='skill-card' style={{ '--j': index + 1 }}>
-                  <Card.Body className='skill-card-body'>
-                    <Card.Title className='skill-card-title'>
-                      <img
-                        className='tech-logo'
-                        src={require(`../../assets/images/technologies/${skill.image}`)}
-                        alt={skill.title}
-                      />
-                    </Card.Title>
-                    <Card.Subtitle className="skill-card-subtitle">{skill.description}</Card.Subtitle>
-                  </Card.Body>
-                </Card>
-              
+              <Card className='skill-card' style={{ '--j': index + 1 }} key={index}>
+                <Card.Body className='skill-card-body'>
+                  <Card.Title className='skill-card-title'>
+                    <img
+                      className='tech-logo'
+                      src={require(`../../assets/images/technologies/${skill.image}`)}
+                      alt={skill.title}
+                    />
+                  </Card.Title>
+                  <Card.Subtitle className="skill-card-subtitle">{skill.description}</Card.Subtitle>
+                </Card.Body>
+              </Card>
             ))}
           </div>
         </Col>
@@ -68,56 +67,51 @@ function SkillSection() {
             className='timeline'
             >
             <TimelineItem>
-                <TimelineOppositeContent color="secondary" className='timeline-year'  style={{ '--j': 2 }}>
-                   <h5 className='timeline-subtitle'><span>06 / 2024</span></h5>
+                <TimelineOppositeContent color="secondary" className='timeline-subtitle'  style={{ '--j': 2 }}>
+                   <span>06 / 2024</span>
                 </TimelineOppositeContent>
                 <TimelineSeparator color = 'secondary'  className='timeline-separator'  style={{ '--j': 1 }}>
                     <TimelineDot color = 'primary'  className='timeline-dot'  style={{ '--j': 1 }}/>
                     <TimelineConnector />
                 </TimelineSeparator>
                 <TimelineContent>
-                    <Typography><h4 className='timeline-title' style={{ '--j': 3 }}>BSc in Computer Science and Technology</h4></Typography>
-                    <Typography><h5 className='timeline-subtitle' style={{ '--j': 4 }}>Tecnológico de Monterrey</h5></Typography>
-                    <Typography>
-                        <p className='timeline-text' style={{ '--j': 5 }}>
+                    <Typography className='timeline-title' style={{ '--j': 3 }}>BSc in Computer Science and Technology</Typography>
+                    <Typography className='timeline-subtitle' style={{ '--j': 4 }}>Tecnológico de Monterrey</Typography>
+                    <Typography className='timeline-text' style={{ '--j': 5 }}>
                         Gained expertise in software engineering, programming languages, data structures, algorithms, and computer systems. Gained hands-on experience in software development, designing, implementing, and testing efficient and scalable solutions. 
-                        </p>
                     </Typography>
                 </TimelineContent>
             </TimelineItem>
             <TimelineItem>
-                <TimelineOppositeContent color="secondary" className='timeline-year'  style={{ '--j': 7 }}>
-                   <h5 className='timeline-subtitle'><span>05 / 2024</span></h5>
+                <TimelineOppositeContent color="secondary" className='timeline-subtitle'  style={{ '--j': 7 }}>
+                   <span>05 / 2024</span>
                 </TimelineOppositeContent>
                 <TimelineSeparator color = 'secondary' className='timeline-separator'  style={{ '--j': 6 }}>
                     <TimelineDot color = 'primary' className='timeline-dot'  style={{ '--j': 6 }}/>
                     <TimelineConnector />
                 </TimelineSeparator>
                 <TimelineContent>
-                    <Typography><h4 className='timeline-title' style={{ '--j': 8 }}>Deep Learning Specialization</h4></Typography>
-                    <Typography><h5 className='timeline-subtitle' style={{ '--j': 9 }}>Huawei Global Training Center</h5></Typography>
-                    <Typography>
-                        <p className='timeline-text' style={{ '--j': 10 }}>
+                    <Typography className='timeline-title' style={{ '--j': 8 }}>Deep Learning Specialization</Typography>
+                    <Typography className='timeline-subtitle' style={{ '--j': 9 }}>Huawei Global Training Center</Typography>
+                    <Typography className='timeline-text' style={{ '--j': 10 }}>
                         Enhanced AI expertise through the Infotec AI 1000 program, gaining proficiency in machine learning, deep learning, and AI tools. Gained hands-on experience in AI development through  applying theoretical knowledge to real-world problems.
-                        </p>
                     </Typography>
                 </TimelineContent>
             </TimelineItem>
             <TimelineItem>
-                <TimelineOppositeContent color="secondary" className='timeline-year'  style={{ '--j': 12 }}>
-                   <h5 className='timeline-subtitle'><span>12 / 2023</span></h5>
+                <TimelineOppositeContent color="secondary" className='timeline-subtitle'  style={{ '--j': 12 }}>
+                   <span>12 / 2023</span>
                 </TimelineOppositeContent>
                 <TimelineSeparator color = 'secondary' className='timeline-separator'  style={{ '--j': 11 }}>
                     <TimelineDot color = 'primary' className='timeline-dot'  style={{ '--j': 11 }}/>
                     <TimelineConnector />
                 </TimelineSeparator>
                 <TimelineContent>
-                    <Typography><h4 className='timeline-title' style={{ '--j': 13 }}>Minor Degree in Data Analytics and Artificial Intelligence</h4></Typography>
-                    <Typography><h5 className='timeline-subtitle' style={{ '--j': 14 }}>Tecnológico de Monterrey</h5></Typography>
-                    <Typography>
-                        <p className='timeline-text' style={{ '--j': 15 }}>
+                    <Typography className='timeline-title' style={{ '--j': 13 }}>Minor Degree in Data Analytics and Artificial Intelligence</Typography>
+                    <Typography className='timeline-subtitle' style={{ '--j': 14 }}>Tecnológico de Monterrey</Typography>
+                    <Typography className='timeline-text' style={{ '--j': 15 }}>
                         Acquired prowess in data analysis techniques, statistical modeling, and machine learning algorithms. Gained practical experience working with various data sources, cleaning, preprocessing, and analyzing data using Python.
-                        </p>
+                        
                     </Typography>
                 </TimelineContent>
             </TimelineItem>
