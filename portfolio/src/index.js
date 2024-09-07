e1903358808007e1c55fe906cb122ed56c040b9a
Augusto Aleman <augustoae09@gmail.com>
2024-09-06 23:45:14 -0600

    Added functionality for better error handling

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import './assets/styles/variables.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from './routes/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- portfolio/src/routes/AppRouter.js
+++ portfolio/src/routes/AppRouter.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import  {HomePage, 
-         AboutPage, 
-         ContactPage,
-         ProjectPage} from '../pages/';
+// Import your page components
+import {
+  HomePage,
+  AboutPage,
+  ContactPage,
+  ProjectPage,
+  NotFound, // Make sure you have created this component
+} from '../pages/';
 
 function AppRouter() {
-    return (
-        <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/contact' element={<ContactPage />} />
-            <Route path='/projects/:projectId' element={<ProjectPage />} /> {/* Add this route */}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/projects/:projectId" element={<ProjectPage />} />
+      <Route path="*" element={<NotFound />} /> {/* Corrected the syntax here */}
+    </Routes>
+  );
 }
 
 export default AppRouter;
